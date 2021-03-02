ef04bcbb6bb886ec895ff11e95072732cf4c2b2c
WITNUS <ehldkwl123@naver.com>
2021-03-02 10:00:29 +0900

    email 오류 잡음

--- src/com/member/action/MemberFindProcessAction.java
+++ src/com/member/action/MemberFindProcessAction.java
@@ -22,29 +22,23 @@ public class MemberFindProcessAction implements Action {
 		String member_tel = request.getParameter("tel1") + "-" + request.getParameter("tel2") + "-" + request.getParameter("tel3");
 		String member_id = request.getParameter("id");
 		String member_email = request.getParameter("email");
-		String member_password = request.getParameter("pass");
 		
-		MemberBean memberbean = new MemberBean();
-		memberbean.setMember_id(member_id);
 		
 		MemberDAO memberdao = new MemberDAO();
-		int result1 = memberdao.isFindName(member_name, member_tel, member_id);
+		String result1 = memberdao.isFindName(member_name, member_tel);
 		System.out.println("결과는" + result1);
 		
 		//Name이 같은경우
-		if(result1 == 1) {
+		if(!result1.equals("")) {
 			response.setContentType("text/html; charset=utf-8");
 			PrintWriter out = response.getWriter();
 			out.println("<script>");
-			out.println("alert('회원님의 아이디는  " + member_id + "  입니다.');");
+			out.println("alert('회원님의 아이디는  " + result1 + "  입니다.');");
 			out.println("location.href='memberFind.com';");
 			out.println("</script>");
-			out.close();
-			return forward;
+			out.close();			
 		}else {
 			String message = "회원이 존재하지 않습니다.";
-			if(result1 == -1)
-				message = "왜그러는거야ㅠㅜ";
 			response.setContentType("text/html; charset=utf-8");
 			PrintWriter out = response.getWriter();
 			out.println("<script>");
@@ -52,8 +46,9 @@ public class MemberFindProcessAction implements Action {
 			out.println("location.href='memberFind.com';");
 			out.println("</script>");
 			out.close();
-			return null;
+			
 		}
+		return null;
 		
 	}
 
